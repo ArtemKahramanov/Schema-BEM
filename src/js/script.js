2dd8785664ddeb15b21b3bb71792b314f5aa533b
Артём Кахраманов <artem@iMac-Artem.local>
2017-11-12 13:27:13 +0300

    ie 10

--- src/js/script.js
+++ src/js/script.js
@@ -27,6 +27,24 @@ $('.menu__butt').on('click', function(){
 	$('.menu__list').toggle(350);
 });
 
-
+function getInternetExplorerVersion() {
+    var rv = -1;
+    if (navigator.appName == 'Microsoft Internet Explorer') {
+        var ua = navigator.userAgent;
+        var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat(RegExp.$1);
+    } else if (navigator.appName == 'Netscape') {
+        var ua = navigator.userAgent;
+        var re = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat(RegExp.$1);
+    }
+    return rv;
+}
+    if (getInternetExplorerVersion() !== -1) {
+        //Значит это IE                    
+        $('.raiting__sum').css('padding', '5px');
+    }
 
 });
